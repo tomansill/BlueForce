90119c1bc229ef1759cf47f1a3553bc4aff13554
tomansill <tomansill@gmail.com>
2015-03-06 13:17:22 -0500

    More cleanup

--- Graph.java
+++ Graph.java
@@ -17,9 +17,7 @@ import java.util.TreeMap;
 import java.util.HashSet;
 import java.util.Map;
 
-enum Operator{
-	LESS_THAN, LESS_THAN_OR_EQUAL, EQUAL, MORE_THAN_OR_EQUAL, MORE_THAN, NOT_EQUAL
-}
+enum Operator{ LESS_THAN, LESS_THAN_OR_EQUAL, EQUAL, MORE_THAN_OR_EQUAL, MORE_THAN, NOT_EQUAL }
 
 public class Graph{
 	/** Record of number of states in the graph */
