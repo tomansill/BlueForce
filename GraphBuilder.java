f3eb1e2524f37541854a82b53323b94717566f37
brian <btp6918@rit.edu>
2015-03-30 17:17:48 -0400

    Improved RandomTest and updated most of Builder functions to work with new design.

--- GraphBuilder.java
+++ GraphBuilder.java
@@ -30,8 +30,8 @@ public class GraphBuilder{
         // Now we randomly connect the vertices...
 
         for (Integer i = 0; i < Math.abs(RNJesus.nextInt(constrict)); i++) {
-            String firstVertex = ((Integer) RNJesus.nextInt(NumOfVertex)).toString();
-            String secondVertex = ((Integer) RNJesus.nextInt(NumOfVertex)).toString();
+            String firstVertex = ((Integer)(Math.abs(RNJesus.nextInt(NumOfVertex)))).toString();
+            String secondVertex = ((Integer)(Math.abs(RNJesus.nextInt(NumOfVertex)))).toString();
             graphified.connectVertices(firstVertex, secondVertex);
         }
         return graphified;
@@ -55,14 +55,15 @@ public class GraphBuilder{
         // Now we randomly connect the vertices...
 
         for (Integer i = 0; i < Math.abs(RNJesus.nextInt()); i++) {
-            String firstVertex = ((Integer) RNJesus.nextInt(NumOfVertex)).toString();
-            String secondVertex = ((Integer) RNJesus.nextInt(NumOfVertex)).toString();
+            String firstVertex = ((Integer)(Math.abs(RNJesus.nextInt(NumOfVertex)))).toString();
+            String secondVertex = ((Integer)(Math.abs(RNJesus.nextInt(NumOfVertex)))).toString();
             graphified.connectVertices(firstVertex, secondVertex);
         }
         return graphified;
     }
 	
-/*	public static Graph buildPathGraph(int vertices){
+/*
+public static Graph buildPathGraph(int vertices){
 		TreeMap<String, Vertex> list = new TreeMap<String, Vertex>();
 		for(int i = 1; i < vertices + 1; i++){
 			String label = convertToReadableLabel(i);
@@ -75,12 +76,25 @@ public class GraphBuilder{
 			if(i != vertices){
 				neighbors.add(convertToReadableLabel(i+1));
 			}
-			list.put(label, new Vertex(label, false, neighbors));
+			list.put(label, new Vertex(label, neighbors));
 		}//End of for loop
 		return new Graph(list);
-	}//End of buildPathGraph method
+	}//End of buildPathGraph method */
+
+    public static Graph buildPathGraph(int vertices) {
+        Graph PathGraph = new Graph();
+        for (int i = 1; i < vertices+1; i++) {
+            String label = convertToReadableLabel(i);
+            PathGraph.addVertex(new Vertex(label));
+            // connect vertices
+            if (i != 1) {
+                PathGraph.connectVertices(label, convertToReadableLabel(i - 1));
+            }
+        }
+        return PathGraph;
+    }
 	
-	public static Graph buildCycleGraph(int vertices){
+	/*public static Graph buildCycleGraph(int vertices){
 		TreeMap<String, Vertex> list = new TreeMap<String, Vertex>();
 		for(int i = 1; i < vertices + 1; i++){
 			String label = convertToReadableLabel(i);
@@ -102,9 +116,25 @@ public class GraphBuilder{
 			list.put(label, new Vertex(label, neighbors, false));
 		}//End of for loop
 		return new Graph(list);
-	}//End of buildCircleGraph method
+	}//End of buildCircleGraph method */
+
+    public static Graph buildCycleGraph(int vertices) {
+        Graph CycleGraph = new Graph();
+        for (int i = 1; i < vertices+1; i++) {
+            String label = convertToReadableLabel(i);
+            CycleGraph.addVertex(new Vertex(label));
+            // connect vertices
+            if (i != 1) {
+                CycleGraph.connectVertices(label, convertToReadableLabel(i - 1));
+            }
+            if (i == vertices) {
+                CycleGraph.connectVertices(convertToReadableLabel(1), label);
+            }
+        }
+        return CycleGraph;
+    }
 	
-	public static Graph buildWheelGraph(int vertices){
+	/*public static Graph buildWheelGraph(int vertices){
 		TreeMap<String, Vertex> list = new TreeMap<String, Vertex>();
 		ArrayList<String> mneighbors = new ArrayList<String>();
 		for(int i = 1; i < vertices; i++){
@@ -131,8 +161,29 @@ public class GraphBuilder{
 		}//End of for loop
 		list.put(convertToReadableLabel(vertices), new Vertex(convertToReadableLabel(vertices), mneighbors, false));
 		return new Graph(list);
-	}//End of buildWheelGraph method
-	
+	}//End of buildWheelGraph method */
+
+    public static Graph buildWheelGraph(int vertices) {
+        Graph WheelGraph = new Graph();
+        String CenterVertex = convertToReadableLabel(1);
+        for (int i = 2; i < vertices+1; i++) {
+            String label = convertToReadableLabel(i);
+            WheelGraph.addVertex(new Vertex(label));
+            // connect current vertex to center
+            WheelGraph.connectVertices(label, CenterVertex);
+            // connect "back" vertex
+            if (i != 2) {
+                WheelGraph.connectVertices(label, convertToReadableLabel(i - 1));
+
+            }
+            if (i == vertices) {
+                WheelGraph.connectVertices(convertToReadableLabel(2), label);
+            }
+        }
+        return WheelGraph;
+    }
+
+/*
 	public static Graph buildBiPartiteGraph(int vertice1, int vertice2){
 		TreeMap<String, Vertex> list = new TreeMap<String, Vertex>();
 		for(int i = 1; i < vertice1+1; i++){
@@ -152,8 +203,31 @@ public class GraphBuilder{
 			list.put(label, new Vertex(label, neighbors, false));
 		}//End of for loop
 		return new Graph(list);
-	}//End of buildBiPartiteGraph method
-	
+	}//End of buildBiPartiteGraph method */
+
+    // NEED to test (it works in theory...)
+	public static Graph buildBiPartiteGraph(int vertice1, int vertice2) {
+        Graph BiPartiteGraph = new Graph();
+        int sum = vertice1+vertice2;
+        for (int i = 1; i < sum; i++) {
+            if (i <= vertice1) {
+                BiPartiteGraph.addVertex(new Vertex("L"+i));
+            } else if ( i == vertice1) {
+                BiPartiteGraph.addVertex(new Vertex("R"+vertice2));
+            }
+            else {
+                BiPartiteGraph.addVertex(new Vertex("R"+(sum-i)));
+            }
+        }
+        for (int i = 1; i < vertice1+1; i++) {
+            for (int j = 1; j < vertice2+1; j++) {
+                BiPartiteGraph.connectVertices("L"+i, "R"+(sum-vertice2+j));
+            }
+        }
+        return BiPartiteGraph;
+    }
+
+	/*
 	public static Graph buildCompleteGraph(int vertice){
 		TreeMap<String, Vertex> list = new TreeMap<String, Vertex>();
 		for(int i = 1; i < vertice + 1; i++){
@@ -168,6 +242,26 @@ public class GraphBuilder{
 		}//End of for loop
 		return new Graph(list);
 	}//End of buildCompleteGraph method
+	*/
+
+    public static Graph buildCompleteGraph(int vertices) {
+        Graph CompleteGraph = new Graph();
+        for (int i = 1; i < vertices+1; i++) {
+            Vertex CurrVertex = new Vertex(convertToReadableLabel(i));
+            CompleteGraph.addVertex(CurrVertex);
+            if (i != 1) {
+                Collection<Vertex> PreviousVerteices = CompleteGraph.getListOfVertices();
+                for (int j = 1; j < PreviousVerteices.size()+1; j++) {
+                    if (j != i) {
+                        CompleteGraph.connectVertices(convertToReadableLabel(i), convertToReadableLabel(j));
+                    }
+                }
+            }
+        }
+        return CompleteGraph;
+    }
+
+    /*
 	public static Graph buildGridGraph(int width, int length){
 		TreeMap<String, Vertex> list = new TreeMap<String, Vertex>();
 		for(int i = 0; i < width; i++){
@@ -187,6 +281,33 @@ public class GraphBuilder{
 		}//End of for loop
 		return new Graph(list);
 	}//End of buildGridGraph method
+*/
+    public static Graph buildGridGraph(int width, int length) {
+        Graph GridGraph = new Graph();
+        // creates the graph with all unconnected vertices
+        for(int i = 0; i < width; i++) {
+            for (int j = 0; j < length; j++) {
+                GridGraph.addVertex(new Vertex("(" + i + "," + j + ")"));
+            }
+        }
+        // connects vertices
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < length; j++){
+                String label = "(" + i + "," + j + ")";
+                //Left side
+                if(i != 0){ GridGraph.connectVertices("(" + (i-1) + "," + j + ")", label); }
+                //Right side
+                if(i != width-1){ GridGraph.connectVertices("(" + (i+1) + "," + j + ")", label); }
+                //Up side
+                if(j != 0){ GridGraph.connectVertices("(" + i + "," + (j-1) + ")", label); }
+                //Down side
+                if(j != length-1){ GridGraph.connectVertices("(" + i + "," + (j+1) + ")", label); }
+            }
+        }
+        return GridGraph;
+    }
+
+    /*
 	public static Graph buildTriangleGridGraph(int numOfVSide){
 		TreeMap<String, Vertex> list = new TreeMap<String, Vertex>();
 		int count = 1;
@@ -213,7 +334,10 @@ public class GraphBuilder{
 		}//End of for loop
 		return new Graph(list);
 	}//End of buildTriangleGridGraph method
-	
+	*/
+
+
+
 	public static String convertToReadableLabel(int number){
 		int carryover = 0;
 		while(true){
@@ -231,5 +355,5 @@ public class GraphBuilder{
 		str += (char)nchar;
 		return str;
 	}//End of convertToReadableLabel method
-	*/
+
 }//End of class
